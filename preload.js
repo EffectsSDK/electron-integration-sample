a1eea102d4ee08483c179975ec164f4e260ad63a
Max Troshin <tmv@tomsksoft.com>
2025-04-10 12:09:13 +0700

    update sdk version

--- preload.js
+++ preload.js
@@ -4,11 +4,23 @@ const tsvb = require("./libs/win/tsvb");
 //const tsvb = require("./libs/mac/tsvb");
 
 const inference = new tsvb.InferenceSession();
+let port;
+
+//this method works started from sdk v3.5.3
+window.addEventListener('message', (event) => {
+     if (event.source !== window) return;
+     if (event.data.type === 'tsvb-custom-inference-port') {
+          port = event.data.port;
+          port.onmessage = async (event) => {
+               const input = new Float32Array(event.data.input);
+               const output = new Float32Array(event.data.output);
+               await inference.run(input, output);
+               port.postMessage({ input, output }, [input.buffer, output.buffer]);  
+          };
+     }
+});
 
 contextBridge.exposeInMainWorld('inferer', {
-     run: (data) => {
-          return inference.run(data);
-     },
      configure: (data) => {
           return inference.configure(data);
      },
